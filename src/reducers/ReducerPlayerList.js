02123970bf96bd5701bd44f83083c6c1bfe89c68
Scottie Crump <scottiecrump@yahoo.com>
2017-05-21 12:34:24 -0400

    updated counter component styles and cleaned up project files

--- src/reducers/ReducerPlayerList.js
+++ src/reducers/ReducerPlayerList.js
@@ -6,15 +6,18 @@ export const ReducerPlayerList = (state = PLAYERS, action) => {
     case ADD_NEW_PLAYER:
       return [...state, action.payload]; // { current players, A new player }
     case UPDATE_PLAYER_SCORE:
-      const updatedItems = state.map((player) => {
-        if (player.id === action.id) {
-          return { ...player, score: action.payload };
-        }
+      {
+        const updatedItems = state.map((player) => {
+          if (player.id === action.id) {
+            return { ...player, score: action.payload }; // { player.score = newScore }
+          }
 
-        return player;
-      });
-      console.log(updatedItems);
-      return updatedItems;
+          return player;
+        });
+        console.log(updatedItems);
+        return updatedItems;
+      }
+
     default:
       return state; // { player 1, player 2, etc. }
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { ReducerPlayerList } from './ReducerPlayerList';
-import { ReducerPlayerScore } from './ReducerPlayerScore';
 
 const rootReducer = combineReducers({
   players: ReducerPlayerList,
-  totalScore: ReducerPlayerScore,
 });
 
 export default rootReducer;
