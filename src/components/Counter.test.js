e9484b704772c6ceedaa6d241974e6e3907c17eb
Scottie Crump <scottiecrump@yahoo.com>
2017-05-31 23:09:08 -0400

    Updates

--- src/components/Counter.test.js
+++
@@ -1,9 +0,0 @@
-/* global document, it:true*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter';
-
-it('renders without crashing', () => {
-  ReactDOM.render(<Counter />, document.createElement('div'));
-});
