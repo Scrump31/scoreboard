9f693ec1a186bba1695e9c178c6f53c32a8f530a
Scottie Crump <sncrump29@gmail.com>
2018-08-23 12:49:57 -0400

    Refactor Player Component - Updated layout - Added updateScore Action

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import Counter from '../containers/Counter';
+import Counter from '../components/Counter';
 
-const Player = ({ removePlayer, name, score, id }) => (
-  <div>
-    <Col md={6} className="player-name">
+const Player = ({ deletePlayer, updateScore, name, score, id }) => (
+  <Row>
+    <Col xs={9} className="player-name">
       <div>
         {name}
         <button
-          onClick={() => removePlayer(id)}
+          onClick={() => deletePlayer(id)}
           className="glyphicon glyphicon-remove"
         />
       </div>
     </Col>
-    <Col md={6} className="player-counter">
-      <Counter id={id} score={score} />
+
+    <Col xs={3} className="player-counter">
+      <Counter id={id} score={score} updateScore={updateScore} />
     </Col>
+
     <Col md={12} className="horizonal-rule" />
-  </div>
+  </Row>
 );
 
 Player.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   score: PropTypes.number,
+  updateScore: PropTypes.func.isRequired,
+  deletePlayer: PropTypes.func.isRequired,
 }.isRequired;
 
 export default Player;
