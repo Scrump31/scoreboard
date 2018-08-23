a5bb9823c3eb9dde64b3d96cbf4a6cbcfe016389
Scottie Crump <sncrump29@gmail.com>
2018-08-23 12:54:26 -0400

    Refactor Counter Component - Downgraded from container to component

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+
+const Counter = ({ id, score, updateScore }) => {
+  const addScore = () => updateScore(score + 1, id);
+  const subtractScore = () => updateScore(score - 1, id);
+
+  return (
+    <div>
+      <Button bsStyle="danger" bsSize="large" onClick={subtractScore}>
+        -
+      </Button>
+      <span className="player-score">{score}</span>
+      <Button bsStyle="success" bsSize="large" onClick={addScore}>
+        +
+      </Button>
+    </div>
+  );
+};
+
+Counter.propTypes = {
+  id: PropTypes.number,
+  score: PropTypes.number,
+  updatePlayerScore: PropTypes.func.isRequired,
+}.isRequired;
+
+export default Counter;
