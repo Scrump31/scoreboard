00dcdbc77caa33188e843bac3929faf04cdf86ed
Scottie Crump <sncrump29@gmail.com>
2018-08-28 08:08:17 -0400

    Updated Counter Test File

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import Counter from './Counter';
 
-it('renders without crashing', () => {
-  ReactDOM.render(<Counter />, document.createElement('div'));
+const ADD_BTN = 'button.btn-success';
+const SUBTRACT_BTN = 'button.btn-danger';
+const SCORE = 'span';
+
+describe('<Counter />', () => {
+  test('renders without crashing', () => {
+    const component = shallow(<Counter />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('displays a score based on props.score', () => {
+    const component = shallow(<Counter score={3} />);
+    expect(component.find(SCORE).text()).toEqual('3');
+  });
+
+  it('calls mockUpdateScore() when "Add" button clicked', () => {
+    const mockUpdateScore = jest.fn();
+    const wrapper = mount(<Counter updateScore={mockUpdateScore} />);
+    wrapper.find(ADD_BTN).simulate('click');
+    expect(mockUpdateScore).toHaveBeenCalled();
+    wrapper.unmount();
+  });
+
+  it('calls mockUpdateScore() when "Subtract" button clicked', () => {
+    const mockUpdateScore = jest.fn();
+    const wrapper = mount(<Counter updateScore={mockUpdateScore} />);
+    wrapper.find(SUBTRACT_BTN).simulate('click');
+    expect(mockUpdateScore).toHaveBeenCalled();
+    wrapper.unmount();
+  });
 });
