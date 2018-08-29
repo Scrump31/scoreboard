10b39093f6680a6c573c4c86fad05e8943bf94aa
Scottie Crump <sncrump29@gmail.com>
2018-08-28 20:45:46 -0400

    Convert App Test To Integration Test - now uses mount() and Root

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import Root from '../Root';
 import App from './App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const component = shallow(<App />);
+    const component = mount(
+      <Root>
+        <App />
+      </Root>,
+    );
     expect(component).toMatchSnapshot();
+    component.unmount();
   });
 });
