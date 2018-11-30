05f5acf1846bcfd83c2c70f64a4b4466f28c5e23
Scottie Crump <sncrump29@gmail.com>
2018-11-30 07:37:59 -0500

    Add Puppeteer UI Tests - upgraded react-scripts - added puppeteer - created UI tests

---
+++ src/integration/addNewPlayer.feature.js
@@ -0,0 +1,18 @@
+const scoreboard = require('./scoreboard');
+
+beforeEach(async () => {
+  await scoreboard.loadApp(false, 100);
+});
+
+afterEach(async () => await scoreboard.closeBrowser());
+
+test('Verify title is "Scoreboard"', async () => {
+  const title = await scoreboard.getScoreboardTitle();
+  expect(title).toEqual('Scoreboard');
+});
+
+test('Verify a new player can be added', async () => {
+  await scoreboard.addNewPlayer('player four');
+  const totalPlayers = await scoreboard.getTotalPlayers();
+  expect(totalPlayers).toEqual('4');
+});
